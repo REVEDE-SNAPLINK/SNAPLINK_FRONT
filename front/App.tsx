0c77c1e28571101c0da62c3fbe052b06f602bf41
geon-2 <gunle0416@gmail.com>
2025-10-08 00:46:43 +0900

    feat: Initialize navigation router and structure

--- front/App.tsx
+++ front/App.tsx
@@ -1,30 +1,16 @@
-import { StatusBar, Text, TouchableOpacity, useColorScheme } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { ThemeProvider, useTheme } from './src/context/ThemeContext.tsx';
-
-function MainScreen() {
-  const { theme } = useTheme();
-  return (
-    <SafeAreaView
-      style={{ flex: 1, backgroundColor: theme.colors.background, justifyContent: 'center', alignItems: 'center' }}
-    >
-      <TouchableOpacity style={{ backgroundColor: theme.colors.primary, justifyContent: 'center', alignItems: 'center', width: 200, height: 40 }}>
-        <Text style={{ color: theme.colors.textPrimary, fontFamily: theme.typography.fontFamily["800"], fontWeight: 800, fontSize: theme.typography.size.xl }}>Hello World!!</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  )
-}
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemeProvider } from '@/context/ThemeContext.tsx';
+import AppNavigator from '@/navigation';
+import { AuthProvider } from '@/context/AuthContext.tsx';
 
 export default function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-  const barStyle = isDarkMode ? 'light-content' : 'dark-content';
-
   return (
     <ThemeProvider>
-      <SafeAreaProvider>
-        <StatusBar barStyle={barStyle} />
-        <MainScreen />
-      </SafeAreaProvider>
+      <AuthProvider>
+        <SafeAreaProvider>
+          <AppNavigator />
+        </SafeAreaProvider>
+      </AuthProvider>
     </ThemeProvider>
   )
 }

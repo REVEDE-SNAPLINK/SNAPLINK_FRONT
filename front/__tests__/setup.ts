5cb3fb41a6da24131465830c05efca1f5b8c2516
geon-2 <gunle0416@gmail.com>
2025-10-16 12:45:32 +0900

    test: Set up test environments

---
+++ front/__tests__/setup.ts
@@ -0,0 +1,22 @@
+import '@testing-library/jest-native/extend-expect';
+
+// Mock AsyncStorage
+jest.mock('@react-native-async-storage/async-storage', () => ({
+  getItem: jest.fn(() => Promise.resolve(null)),
+  setItem: jest.fn(() => Promise.resolve()),
+  removeItem: jest.fn(() => Promise.resolve()),
+}));
+
+// Mock react-native-reanimated
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+  Reanimated.default.call = () => {};
+  return Reanimated;
+});
+
+// Silence the warning: Animated: `useNativeDriver` is not supported
+global.console = {
+  ...console,
+  warn: jest.fn(),
+  error: jest.fn(),
+};

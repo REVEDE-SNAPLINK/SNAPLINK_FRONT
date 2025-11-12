912500eef3f6f818f9547b7c9a7b3c1abebb0259
geon-2 <gunle0416@gmail.com>
2025-11-12 19:12:49 +0900

    feat: Add BottomNavigation and User-specific routing structure

--- front/src/screens/auth/Login/LoginContainer.tsx
+++ front/src/screens/auth/Login/LoginContainer.tsx
@@ -3,16 +3,15 @@ import React from 'react';
 import LoginView from './LoginView';
 import { User } from '@/types/auth.ts';
 import { useNavigation } from '@react-navigation/native';
-import {AuthNavigationProp, MainNavigationProp} from '@/types/navigation';
+import { AuthNavigationProp } from '@/types/navigation';
 
 export default function LoginContainer() {
   const { signIn } = useAuth();
   const authNavigation = useNavigation<AuthNavigationProp>();
-  const mainNavigation = useNavigation<MainNavigationProp>();
 
   const onLogin = async (apiUrl: string) => {
     console.log(apiUrl);
-    const response = { isAllowed: false };
+    const response = { isAllowed: true };
     let token = "asdf";
     const user: User = {
       id: '1',
@@ -21,9 +20,7 @@ export default function LoginContainer() {
       userType: 'user',
     }
     signIn(token, user, !response.isAllowed);
-    if (response.isAllowed) {
-      mainNavigation.navigate('Home');
-    } else {
+    if (!response.isAllowed) {
       authNavigation.navigate('SelectType')
     }
   }
@@ -32,10 +29,10 @@ export default function LoginContainer() {
     await onLogin('/api/v1/auth/kakao/login');
   }
   const handleNaverLogin = async () => {
-    await onLogin('/api/v1/auth/kakao/login');
+    await onLogin('/api/v1/auth/naver/login');
   }
   const handleGoogleLogin = async () => {
-    await onLogin('/api/v1/auth/kakao/login');
+    await onLogin('/api/v1/auth/google/login');
   }
 
   return <LoginView

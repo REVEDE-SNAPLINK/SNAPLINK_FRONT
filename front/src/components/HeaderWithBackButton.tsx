747003242150608cca73d8a218e8b8bb87a02553
geon-2 <gunle0416@gmail.com>
2025-11-14 22:20:59 +0900

    feat: Add header in ScreenContainer

---
+++ front/src/components/HeaderWithBackButton.tsx
@@ -0,0 +1,67 @@
+import styled from '@/utils/scale/CustomStyled.ts';
+import BackButton from '@/components/BackButton.tsx';
+import Typography from '@/components/theme/Typography.tsx';
+import IconButton from '@/components/IconButton.tsx';
+import { SvgProps } from 'react-native-svg';
+import { ComponentType } from 'react';
+
+interface HeaderWithBackButtonProps {
+  onPressBackButton: () => void;
+  title: string;
+  ToolIcon?: ComponentType<SvgProps>;
+  onPressToolButton?: () => void;
+}
+
+export default function HeaderWithBackButton({
+  onPressBackButton,
+  title,
+  ToolIcon,
+  onPressToolButton,
+}: HeaderWithBackButtonProps) {
+  return (
+    <Container>
+      <BackButtonWrapper>
+        <BackButton onPress={onPressBackButton} />
+      </BackButtonWrapper>
+      <Typography
+        fontSize={20}
+        fontWeight="bold"
+        lineHeight="140%"
+        letterSpacing="-2.5%"
+      >
+        {title}
+      </Typography>
+      <ToolButtonWrapper>
+        {ToolIcon !== undefined && onPressToolButton && (
+          <IconButton width={24} height={24} Svg={ToolIcon} onPress={onPressToolButton} />
+        )}
+      </ToolButtonWrapper>
+    </Container>
+  )
+}
+
+const Container = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  height: 101px;
+  background-color: #fff;
+  width: 100%;
+  padding-bottom: 20px;
+`
+
+const BackButtonWrapper = styled.View`
+  position: absolute;
+  left: 20px;
+  top: 0;
+  height: 100%;
+  justify-content: center;
+`
+
+const ToolButtonWrapper = styled.View`
+  position: absolute;
+  right: 20px;
+  top: 0;
+  height: 100%;
+  justify-content: center;
+`
